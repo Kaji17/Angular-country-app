fffe70f961058a5a16709d0b30dd0307942dcc7a
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-12-07 16:05:55 +0000

    recuperer la liste des pays du services CountryLisServices

--- src/app/countries/country-list/country-list.component.ts
+++ src/app/countries/country-list/country-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryListService } from '../shared/service/countryList.service';
 
 @Component({
   selector: 'app-country-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryListComponent implements OnInit {
 
-  constructor() { }
+  public countries: any[] = [];
+  constructor(
+    private countryList: CountryListService
+  ) { }
 
   ngOnInit(): void {
+    this.countryList.getCountries().subscribe({
+      next: countries=>{
+        this.countries=countries
+      }
+    })
+  }
+
+  /**
+   * displayMenu
+   */
+  public displayMenu(): void {
+    
   }
 
 }
