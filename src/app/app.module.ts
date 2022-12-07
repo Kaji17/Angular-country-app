90c73aa5e1da27755ea270b41a60115f55085209
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-12-07 00:12:47 +0000

    add component header country-list in app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CountryListComponent } from './countries/country-list/country-list.component';
+import { CountryDetailComponent } from './countries/country-detail/country-detail.component';
+import { DarkLigthModeComponent } from './shared/component/dark-ligth-mode/dark-ligth-mode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CountryListComponent,
+    CountryDetailComponent,
+    DarkLigthModeComponent
   ],
   imports: [
     BrowserModule
